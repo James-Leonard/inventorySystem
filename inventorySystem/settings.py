3a90c0b06fddf28e846561c7bb16d1f300852e38
james-leonard <merrymerry1320@gmail.com>
2022-12-31 16:34:13 +0100

    changes to middleware in setting file

--- inventorySystem/settings.py
+++ inventorySystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jg4)g+-g3b_x+skoki7v80k*^e4c3i_-qu%xy!mnj^29%z3^9-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'https://https://inventoryfirst.up.railway.app/']
+ALLOWED_HOSTS = ['*', 'https://inventoryfirst.up.railway.app/']
 #CSRF_TRUSTED_ORIGINS = ['https://inventorysystem-production.up.railway.app']
 
 
